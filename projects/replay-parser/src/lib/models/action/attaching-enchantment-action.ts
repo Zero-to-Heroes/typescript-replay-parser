20501b47b78c3585c9c82b77083d33c545ec5fb5
sebastientromp <sebastien.tromp@gmail.com>
2019-10-19 11:13:21 +0200

    don't crash when attached to entity is not defined in XML

--- projects/replay-parser/src/lib/models/action/attaching-enchantment-action.ts
+++ projects/replay-parser/src/lib/models/action/attaching-enchantment-action.ts
@@ -7,50 +7,49 @@ import { Action } from './action';
 import { HasTargets } from './has-targets';
 
 export class AttachingEnchantmentAction extends Action implements HasTargets {
-  readonly originId: number;
-  readonly enchantmentCardId: string;
-  readonly targetIds: readonly number[];
+	readonly originId: number;
+	readonly enchantmentCardId: string;
+	readonly targetIds: readonly number[];
 
-  readonly allCards: AllCardsService;
+	readonly allCards: AllCardsService;
 
-  constructor(allCards: AllCardsService) {
-	super();
-	this.allCards = allCards;
-  }
+	constructor(allCards: AllCardsService) {
+		super();
+		this.allCards = allCards;
+	}
 
-  public static create(
-	newAction,
-	allCards: AllCardsService
-  ): AttachingEnchantmentAction {
-	return Object.assign(new AttachingEnchantmentAction(allCards), newAction);
-  }
+	public static create(newAction, allCards: AllCardsService): AttachingEnchantmentAction {
+		return Object.assign(new AttachingEnchantmentAction(allCards), newAction);
+	}
 
-  public update(entities: Map<number, Entity>): AttachingEnchantmentAction {
-	return Object.assign(new AttachingEnchantmentAction(this.allCards), this, {
-		entities
-	});
-  }
+	public update(entities: Map<number, Entity>): AttachingEnchantmentAction {
+		return Object.assign(new AttachingEnchantmentAction(this.allCards), this, {
+			entities,
+		});
+	}
 
-  public enrichWithText(): AttachingEnchantmentAction {
-	const creatorCardId = ActionHelper.getCardId(this.entities, this.originId);
-	const creatorCard = this.allCards.getCard(creatorCardId);
-	const enchantmentCard = this.allCards.getCard(this.enchantmentCardId);
-	const targetCardNames = this.targetIds
-		.map(targetId => this.entities.get(targetId))
-		.map(targetEntity =>
-		targetEntity.cardID
-			? this.allCards.getCard(targetEntity.cardID).name
-			: // Enchantments sometimes target the player itself, not the hero
-			(targetEntity as PlayerEntity).name
-		)
-		.join(', ');
-	const textRaw = `\t${creatorCard.name} enchants ${targetCardNames} with ${enchantmentCard.name}`;
-	return Object.assign(new AttachingEnchantmentAction(this.allCards), this, {
-		textRaw
-	});
-  }
+	public enrichWithText(): AttachingEnchantmentAction {
+		const creatorCardId = ActionHelper.getCardId(this.entities, this.originId);
+		const creatorCard = this.allCards.getCard(creatorCardId);
+		const enchantmentCard = this.allCards.getCard(this.enchantmentCardId);
+		const targetCardNames = this.targetIds
+			.map(targetId => this.entities.get(targetId))
+			// There are some weird cases where the entity is not defined in the XML itself
+			.filter(targetEntity => targetEntity)
+			.map(targetEntity =>
+				targetEntity.cardID
+					? this.allCards.getCard(targetEntity.cardID).name
+					: // Enchantments sometimes target the player itself, not the hero
+					  (targetEntity as PlayerEntity).name,
+			)
+			.join(', ');
+		const textRaw = `\t${creatorCard.name} enchants ${targetCardNames} with ${enchantmentCard.name}`;
+		return Object.assign(new AttachingEnchantmentAction(this.allCards), this, {
+			textRaw,
+		});
+	}
 
-  protected getInstance(): Action {
-	return new AttachingEnchantmentAction(this.allCards);
-  }
+	protected getInstance(): Action {
+		return new AttachingEnchantmentAction(this.allCards);
+	}
 }
--- projects/replay-parser/src/lib/models/action/card-target-action.ts
+++ projects/replay-parser/src/lib/models/action/card-target-action.ts
@@ -6,43 +6,43 @@ import { Action } from './action';
 import { HasTargets } from './has-targets';
 
 export class CardTargetAction extends Action implements HasTargets {
-  readonly originId: number;
-  readonly targetIds: readonly number[];
+	readonly originId: number;
+	readonly targetIds: readonly number[];
 
-  readonly allCards: AllCardsService;
+	readonly allCards: AllCardsService;
 
-  constructor(allCards: AllCardsService) {
-	super();
-	this.allCards = allCards;
-  }
+	constructor(allCards: AllCardsService) {
+		super();
+		this.allCards = allCards;
+	}
 
-  public static create(newAction, allCards: AllCardsService): CardTargetAction {
-	return Object.assign(new CardTargetAction(allCards), newAction);
-  }
+	public static create(newAction, allCards: AllCardsService): CardTargetAction {
+		return Object.assign(new CardTargetAction(allCards), newAction);
+	}
 
-  public update(entities: Map<number, Entity>): CardTargetAction {
-	return Object.assign(new CardTargetAction(this.allCards), this, {
-		entities
-	});
-  }
+	public update(entities: Map<number, Entity>): CardTargetAction {
+		return Object.assign(new CardTargetAction(this.allCards), this, {
+			entities,
+		});
+	}
 
-  public enrichWithText(): CardTargetAction {
-	const originCardId = ActionHelper.getCardId(this.entities, this.originId);
-	const targetCardIds = this.targetIds.map(entityId =>
-		ActionHelper.getCardId(this.entities, entityId)
-	);
-	const originCardName = this.allCards.getCard(originCardId).name;
-	const targetCardNames = targetCardIds
-		.map(cardId => this.allCards.getCard(cardId))
-		.map(card => card.name)
-		.join(', ');
-	const textRaw = `\t${originCardName} targets ${targetCardNames}`;
-	return Object.assign(new CardTargetAction(this.allCards), this, {
-		textRaw
-	});
-  }
+	public enrichWithText(): CardTargetAction {
+		const originCardId = ActionHelper.getCardId(this.entities, this.originId);
+		const targetCardIds = this.targetIds.map(entityId => ActionHelper.getCardId(this.entities, entityId));
+		const originCardName = this.allCards.getCard(originCardId).name;
+		const targetCardNames = targetCardIds
+			.map(cardId => this.allCards.getCard(cardId))
+			// There are some weird cases where the entity is not defined in the XML itself
+			.filter(card => card)
+			.map(card => card.name)
+			.join(', ');
+		const textRaw = `\t${originCardName} targets ${targetCardNames}`;
+		return Object.assign(new CardTargetAction(this.allCards), this, {
+			textRaw,
+		});
+	}
 
-  protected getInstance(): Action {
-	return new CardTargetAction(this.allCards);
-  }
+	protected getInstance(): Action {
+		return new CardTargetAction(this.allCards);
+	}
 }
